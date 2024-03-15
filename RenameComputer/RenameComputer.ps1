c30b5c6143d31170813cf0054fb0365acd7387d9
Michael Niehaus <niehaus@live.com>
2024-03-15 11:12:54 -0700

    Add support to only rename computers with a matching prefix

--- RenameComputer/RenameComputer.ps1
+++ RenameComputer/RenameComputer.ps1
@@ -1,7 +1,7 @@
 
 <#PSScriptInfo
 
-.VERSION 1.2
+.VERSION 1.3
 
 .GUID 3b42d8c8-cda5-4411-a623-90d812a8e29e
 
@@ -26,6 +26,7 @@
 .EXTERNALSCRIPTDEPENDENCIES
 
 .RELEASENOTES
+Version 1.3: Only rename if the current name's prefix matches
 Version 1.2: Fixed PowerShell 5.1 bug (BiosSeralNumber)
 Version 1.1: Updated for AAD scenarios too.
 Version 1.0: Initial version.
@@ -42,15 +43,17 @@ Note that if the device is joined to AD, there must be connectivity to the domai
 
 #> 
 
-Param()
-
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory = $False)] [string] $Prefix = ""
+)
 
 # If we are running as a 32-bit process on an x64 system, re-launch as a 64-bit process
 if ("$env:PROCESSOR_ARCHITEW6432" -ne "ARM64")
 {
     if (Test-Path "$($env:WINDIR)\SysNative\WindowsPowerShell\v1.0\powershell.exe")
     {
-        & "$($env:WINDIR)\SysNative\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy bypass -File "$PSCommandPath"
+        & "$($env:WINDIR)\SysNative\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy bypass -File "$PSCommandPath" -Prefix $Prefix
         Exit $lastexitcode
     }
 }
@@ -70,21 +73,34 @@ if (-not (Test-Path $dest))
 }
 Start-Transcript "$dest\RenameComputer.log" -Append
 
-# See if we are AD or AAD joined
+# Bail out if the prefix doesn't match (if specified)
+Write-Host $Prefix
 $details = Get-ComputerInfo
+if (($Prefix -ne "") -and (-not $details.CsName.StartsWith($Prefix))) {
+    Write-Host "Device name doesn't match specified prefix, bailing out. Prefix=$Prefix ComputerName=$($details.CsName)"
+    Stop-Transcript
+    Exit 0
+}
+Write-Host "Oh!"
+exit 3
+
+# See if we are AD or AAD joined
 $isAD = $false
 $isAAD = $false
+$tenantID = $null
 if ($details.CsPartOfDomain) {
     Write-Host "Device is joined to AD domain: $($details.CsDomain)"
     $isAD = $true
     $goodToGo = $false
 } else {
     $goodToGo = $true
-    $subKey = Get-Item "HKLM:/SYSTEM/CurrentControlSet/Control/CloudDomainJoin/JoinInfo"
-    $guids = $subKey.GetSubKeyNames()
-    foreach($guid in $guids) {
-        $guidSubKey = $subKey.OpenSubKey($guid);
-        $tenantId = $guidSubKey.GetValue("TenantId");
+    if (Test-Path "HKLM:/SYSTEM/CurrentControlSet/Control/CloudDomainJoin/JoinInfo") {
+        $subKey = Get-Item "HKLM:/SYSTEM/CurrentControlSet/Control/CloudDomainJoin/JoinInfo"
+        $guids = $subKey.GetSubKeyNames()
+        foreach($guid in $guids) {
+            $guidSubKey = $subKey.OpenSubKey($guid);
+            $tenantId = $guidSubKey.GetValue("TenantId");
+        }
     }
     if ($null -ne $tenantID) {
         Write-Host "Device is joined to AAD tenant: $tenantID"
